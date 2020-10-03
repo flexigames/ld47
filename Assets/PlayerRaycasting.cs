1397e3505bf6241cc6453ab8b1d3ff15933a6393
finn <derfinn@gmail.com>
2020-10-03 17:32:05 +0200

    Add a debug raycast for the player

---
+++ Assets/PlayerRaycasting.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRaycasting : MonoBehaviour
+{
+    public float distanceToInteract;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        Debug.DrawRay(this.transform.position, this.transform.forward * distanceToInteract, Color.magenta);
+    }
+}
