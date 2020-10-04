7f00992e63b6bb1094e791591102c2a76a0c0f55
Finn Pauls <mbp@pauls.dev>
2020-10-04 20:02:01 +0200

    Make tree choppable with axe

---
+++ Assets/ChopTarget.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChopTarget : Interactable
+{
+    public override void OnInteract()
+    {
+        var animator = GetComponent<Animator>();
+        animator.enabled = true;
+        
+        var player = GameObject.FindWithTag("Player");
+        var carrying = player.GetComponent<Carrying>();
+        carrying.itemType = null;
+        Destroy(carrying.itemGameObject);
+        
+        Destroy(this);
+    }
+    
+    public override bool isActive()
+    {
+        var player = GameObject.FindWithTag("Player");
+        if (!player) return false;
+        var carrying = player.GetComponent<Carrying>();
+        
+        return carrying.itemType == "Axe";
+    }
+}
