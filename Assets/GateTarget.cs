35cbb29d86b140a1eabc82901c9115ef7eb9abae
Finn Pauls <mbp@pauls.dev>
2020-10-04 23:04:19 +0200

    Add Gate Logic

---
+++ Assets/GateTarget.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateTarget : Target
+{
+    public override void OnInteract()
+    {
+        this.DisableOutline();
+        
+        var player = GameObject.FindWithTag("Player");
+        var carrying = player.GetComponent<Carrying>();
+        carrying.itemType = null;
+        Destroy(carrying.itemGameObject);
+
+        resolved = true;
+        
+        Destroy(gameObject);
+        Destroy(this);
+    }
+    
+    public override bool isActive()
+    {
+        var player = GameObject.FindWithTag("Player");
+        if (!player) return false;
+        var carrying = player.GetComponent<Carrying>();
+        return carrying.itemType == "Key";
+    }
+}
