44553bc847eb8d8dca10728beb8152e9f76bc113
Finn Pauls <mbp@pauls.dev>
2020-10-05 21:06:28 +0200

    Make ghost happy by hovering over play

--- Assets/GhostMenu.cs
+++ Assets/GhostMenu.cs
@@ -6,8 +6,12 @@ using UnityEngine.SceneManagement;
 public class GhostMenu : MonoBehaviour
 {
     public Camera camera;
+    public MeshRenderer ghostMesh;
+    public Material ghostMaterialHappy;
+    public Material ghostMaterialSad;
 
     private Outline outlineHover;
+
     void Update()
     {
         RaycastHit hit;
@@ -15,6 +19,7 @@ public class GhostMenu : MonoBehaviour
         if (!Physics.Raycast(ray, out hit))
         {
             if (outlineHover) outlineHover.OutlineWidth = 0;
+            ghostMesh.material = ghostMaterialSad;
             return;
         }
         
@@ -24,12 +29,15 @@ public class GhostMenu : MonoBehaviour
         if (objectName != "PlayButton")
         {
             if (outlineHover) outlineHover.OutlineWidth = 0;
+            ghostMesh.material = ghostMaterialSad;
             return;
         }
 
         var outline = objectHit.gameObject.GetComponent<Outline>();
         outline.OutlineWidth = 9;
         outlineHover = outline;
+
+        ghostMesh.material = ghostMaterialHappy;
         
         
         if (Input.GetMouseButtonDown(0))
